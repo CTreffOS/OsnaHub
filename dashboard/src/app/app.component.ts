16ed9b7a8582714c331d2c0ec72a212c5b6955a1
Kevin Rüter <k.rueter93@gmail.com>
2020-06-26 11:26:44 +0200

    added setting position of module cards based on last drop

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ISettings } from './models/ISettings';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,45 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Dashboard | OsnaHub';
 
-  modules: object;
+  settings: ISettings;
 
   constructor() {
-    this.modules = {
-      bahn: true,
-      weather: false,
-      trash: true,
-      ris: true
+    // set default settings
+    this.settings = {
+      modules: {
+        ris: {
+          active: true,
+          position: { x: 0, y: 0 }
+        },
+        bahn: {
+          active: true,
+          position: { x: 0, y: 0 }
+        },
+        trash: {
+          active: true,
+          position: { x: 0, y: 0 }
+        }
+      }
     };
+
+    this.loadSettings();
+  }
+
+  loadSettings() {
+    const settingsStr = localStorage.getItem('settings');
+    if (settingsStr) {
+      this.settings = JSON.parse(settingsStr);
+    }
+  }
+
+  saveSettings() {
+    localStorage.setItem('settings', JSON.stringify(this.settings));
+  }
+
+  drop($event: any, key: string) {
+    this.settings.modules[key].position.x += $event.distance.x;
+    this.settings.modules[key].position.y += $event.distance.y;
+
+    this.saveSettings();
   }
 }
