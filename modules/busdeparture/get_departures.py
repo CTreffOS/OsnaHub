7f7c36b91ecfbe17f15cc81f78f9127d90429653
elanfer <elanfer@uni-osnabrueck.de>
2020-06-26 03:18:36 +0200

    added pretty tables and ncol argument

--- modules/busdeparture/get_departures.py
+++ modules/busdeparture/get_departures.py
@@ -11,12 +11,23 @@ license: GPLv3.0
 import sys
 import json
 import requests
+import prettytable
 
 # API Endpoint
 url = 'https://fahrplan.vos.info/bin/mgate.exe'
 
+
+# Check program arguments
+if len(sys.argv) == 3:
+    ncols = sys.argv[2]
+elif len(sys.argv) == 2:
+    ncols = '15'
+else:
+    print('USAGE: get_departures.py STATION TABLELENGTH(default: 15)')
+
 # use search string from arguments to query the api for the correct station identifier
 station_query = sys.argv[1]
+
 payload = json.loads('{"id":"zugcwvduies3y4cc","ver":"1.32","lang":"deu","auth":{"type":"AID","aid":"PnYowCQP7Tp1V"},"client":{"id":"SWO","type":"WEB","name":"webapp","l":"vs_swo"},"formatted":false,"svcReqL":[{"req":{"input":{"field":"S","loc":{"name":"Osnabrück '+ station_query +'?","type":"S","dist":1000},"maxLoc":1}},"meth":"LocMatch","id":"1|6|"}]}')
 headers = {
   'Content-Type': 'application/json'
@@ -33,7 +44,7 @@ station_lid = station_query_results['svcResL'][0]['res']['match']['locL'][0]['li
 station_name = station_query_results['svcResL'][0]['res']['match']['locL'][0]['name']
 
 # Query departure times
-payload = json.loads('{"id":"nsk88vbu226fy6c4","ver":"1.32","lang":"deu","auth":{"type":"AID","aid":"PnYowCQP7Tp1V"},"client":{"id":"SWO","type":"WEB","name":"webapp","l":"vs_swo"},"formatted":false,"svcReqL":[{"req":{"stbLoc":{"name":"Osnabrück Kalkhügel","lid":"'+ station_lid +'"},"jnyFltrL":[{"type":"PROD","mode":"INC","value":1023}],"type":"DEP","sort":"PT","maxJny":40},"meth":"StationBoard","id":"1|9|"}]}')
+payload = json.loads('{"id":"nsk88vbu226fy6c4","ver":"1.32","lang":"deu","auth":{"type":"AID","aid":"PnYowCQP7Tp1V"},"client":{"id":"SWO","type":"WEB","name":"webapp","l":"vs_swo"},"formatted":false,"svcReqL":[{"req":{"stbLoc":{"name":"Osnabrück Kalkhügel","lid":"'+ station_lid +'"},"jnyFltrL":[{"type":"PROD","mode":"INC","value":1023}],"type":"DEP","sort":"PT","maxJny":'+ ncols +'},"meth":"StationBoard","id":"1|9|"}]}')
 headers = {
   'Content-Type': 'application/json'
 }
@@ -42,6 +53,10 @@ json_data = json.loads(response.text.encode('utf8'))
 
 print('Departure times for ' + station_name)
 
+table = prettytable.PrettyTable()
+table.field_names = ['Line', 'Destination', 'Departure time', 'Delay', 'Information']
+table.align = 'l'
+
 # itereate over all departures
 for i, item in enumerate(json_data['svcResL'][0]['res']['jnyL']):
     # prodX is used as the foreign key for the departure and references to the array position in producs (bus lines)
@@ -49,9 +64,10 @@ for i, item in enumerate(json_data['svcResL'][0]['res']['jnyL']):
     destination = item['dirTxt']
     # parse departure time
     departure = item['stbStop']['dTimeS'][0:2] + ':' + item['stbStop']['dTimeS'][2:4]
-    print('[' + line + '] ' + destination + ' -  ' + departure)
+    delay = 0
     
     # check if there are any warnings
+    messages_array = []
     if 'himIdL' in json_data['svcResL'][0]['res']['common']['prodL'][item['prodL'][0]['prodX']].keys():
         # iterate over all available warnings for the product/line and check if we have a custom text for that
         for msg_id in json_data['svcResL'][0]['res']['common']['prodL'][item['prodL'][0]['prodX']]['himIdL']:
@@ -59,5 +75,7 @@ for i, item in enumerate(json_data['svcResL'][0]['res']['jnyL']):
                 # the warning id is not just an id, it begins with HIM_FREETEXT_ so we have to split it to get the integer value
                 if int(message['hid']) == int(msg_id.split('_')[2]):
                     # print short title, long version is in field text
-                    print('  ' + message['head'])
+                    messages_array.append(message['head'])
                     # print('  ' + message['text'])
+    table.add_row([line, destination, departure, delay, messages_array])
+print(table)
