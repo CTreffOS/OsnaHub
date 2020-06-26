e331f091aafafcbded8acc85dc04e66be0e8882f
Kevin Rüter <k.rueter93@gmail.com>
2020-06-26 14:32:48 +0200

    updated prod environment

--- dashboard/src/environments/environment.prod.ts
+++ dashboard/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrls: {
+    bahn: 'https://marudor.de/api/',
+    mastodon: 'https://osna.social/api/v1/',
+    trash: 'https://geo.osnabrueck.de/osb-service/abfuhrkalender/',
+    bus: 'https://hub.osna.social/api/bus/'
+  },
+  mastodonAccounts: {
+    ris: 756,
+    risDecisions: 770
+  }
 };
